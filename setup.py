0be5b7f20d034d5f35e17b1fe47dff5633922c81
Artem3213212 <cx-1212@yandex.ru>
2024-07-04 19:22:31 +0300

    fix CI

--- setup.py
+++ setup.py
@@ -1,1 +1,20 @@
-import pathlibfrom setuptools import setup_BASE_PATH = pathlib.Path(__file__).parentsetup(    name="flymyai",    version="$FLYMYAI_PYTHON_VERSION",    packages=[        "core",        "utils",        "multipart",    ],    package_dir={"": str((_BASE_PATH / "flymyai"))},    license="",    author="oleg",    author_email="lyerhd@gmail.com",    description="",)
+import pathlib
+
+from setuptools import setup
+
+_BASE_PATH = pathlib.Path(__file__).parent
+
+setup(
+    name="flymyai",
+    version="$FLYMYAI_PYTHON_VERSION",
+    packages=[
+        "core",
+        "utils",
+        "multipart",
+    ],
+    package_dir={"": str((_BASE_PATH / "flymyai"))},
+    license="",
+    author="oleg",
+    author_email="lyerhd@gmail.com",
+    description="",
+)
